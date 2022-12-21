b3726ec9e2328c89726bb067ddc60e5724da4e94
QChWnd <qchwnd@gmail.com>
2022-12-21 17:52:48 +0800

    del png & css

---
+++ cdn/js/autobg.js
@@ -0,0 +1,4 @@
+if (window.screen.width < window.screen.height)
+    document.body.style.background = 'url(https://cdn.statically.io/gh/QChWnd/QChWnd.GitHub.io/85fe6172/cdn/bg/y' + Math.floor(Math.random() * 100) + '.webp) center top / cover no-repeat fixed';
+else
+    document.body.style.background = 'url(https://cdn.statically.io/gh/QChWnd/QChWnd.GitHub.io/85fe6172/cdn/bg/x' + Math.floor(Math.random() * 103) + '.webp) center top / cover no-repeat fixed';
